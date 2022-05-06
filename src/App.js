4b9d19fa14e10e5713dd08c6b405df1fccdeaa85
nishantatgit <nishant.kumar.shreevastav@gmail.com>
2022-05-06 22:58:51 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Build in React
         </a>
       </header>
     </div>
